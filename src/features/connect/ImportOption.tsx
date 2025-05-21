8cda6498965da486a1a840b857c3ff34ca118e47
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2025-05-21 16:56:01 -0300

    Add recovery phrase input

--- src/features/connect/ImportOption.tsx
+++ src/features/connect/ImportOption.tsx
@@ -17,18 +17,86 @@ export const ImportOption = () => {
         Import an existing wallet
       </Option>
       <Modal ref={modalRef}>
-        <div className="max-w-80 flex flex-col">
-          <h2 className="text-lg font-semibold text-stone-50">
-            🔑 Import a Wallet
-          </h2>
-          <p className="text-stone-400 text-sm w-full text-left mt-2 pl-1">
-            Enter your private key or seed phrase to import an existing wallet.
-          </p>
-          <button className="mt-4 w-full p-2 bg-stone-600 text-stone-50 rounded-lg hover:bg-stone-500 transition-all duration-400">
-            Import
-          </button>
-        </div>
+        <ImportModal />
       </Modal>
     </>
   );
+};
+
+const ImportModal = () => {
+  return (
+    <div>
+      <h2>Recovery Phrase</h2>
+      <p className="mt-1">
+        Import wallets from an existing account with your 12-word recovery
+        phrase
+      </p>
+      <div className="grid grid-cols-3 gap-2 min-w-90 mt-4">
+        {new Array(12).fill(null).map((_, index) => (
+          <div
+            key={`word-${index}`}
+            className="bg-stone-950 border rounded-md p-2 flex items-center border-stone-600 gap-1 has-focus:border-stone-300 text-stone-600  has-focus:text-stone-300"
+            onKeyDown={(e) => {
+              if (index !== 11 && ["Enter", "Space", "Tab"].includes(e.key)) {
+                const input = document.getElementById(
+                  `input-${index}`
+                ) as HTMLInputElement;
+
+                input.focus();
+              }
+            }}
+          >
+            <span className="text-sm font-bold">{`${index + 1}.`}</span>
+            <div className="flex-1 overflow-x-scroll no-scrollbar">
+              <input
+                id={`input-${index}`}
+                type="text"
+                className="w-max"
+                onChange={handleInput}
+                onKeyDown={(e) => {
+                  if (
+                    index !== 11 &&
+                    ["Enter", "Space", "Tab"].includes(e.key)
+                  ) {
+                    const input = document.getElementById(
+                      `input-${index}`
+                    ) as HTMLInputElement;
+
+                    input.focus();
+                  }
+                }}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={() => {}}
+        className="mt-4 w-full p-2 bg-stone-600 text-stone-50 rounded-lg hover:bg-stone-500 transition-all duration-400"
+      >
+        Add account
+      </button>
+    </div>
+  );
+};
+
+const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const value = e.currentTarget.value;
+  const words = value.trimStart().split(" ");
+
+  e.currentTarget.value = words.shift()?.trim() || "";
+
+  let nextIndex = parseInt(e.currentTarget.id.split("-")[1], 10) + 1;
+
+  while (words.length) {
+    const nextInput = document.getElementById(
+      `input-${nextIndex}`
+    ) as HTMLInputElement;
+
+    if (nextInput) {
+      nextInput.focus();
+      nextInput.value = words.shift()?.trim() ?? "";
+      nextIndex++;
+    }
+  }
 };
