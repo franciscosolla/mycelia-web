c3b96116541bb81ba4deec11467a353c1f0580a8
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2025-05-23 00:46:35 -0300

    update

--- src/features/accounts/AccountButton.tsx
+++ src/features/accounts/AccountButton.tsx
@@ -17,12 +17,12 @@ export const AccountButton = ({
         className="bg-stone-50 rounded-full p-4 aspect-square text-center flex flex-col items-center justify-center font-black"
         onClick={onClick}
       >
-        {account.symbol ?? `A${account.id.slice(-1)}`}
+        {`A${account.ethereum.slice(-1)}`}
       </button>
       <div className="group-hover:block hidden absolute top-0 right-full pr-2 text-stone-50">
         <article className="flex flex-col gap-2 bg-stone-900 rounded-sm p-2 text-sm">
           <h1 className="text-center font-bold">
-            {account.name ?? `Account ${account.id.slice(-1)}`}
+            {account.name ?? `Account ${account.ethereum.slice(-1)}`}
           </h1>
           <Network network="ethereum" address={account.ethereum} />
         </article>
